7a478d6050430bbba4b29de2b9bbebd78593091f
viktorov <viktorovvadim186@gmail.com>
2023-03-16 13:23:48 +0300

    3

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        double x = double.Parse(Console.ReadLine());
+
+        double result = (Math.Sin(x) + Math.Cos(x)) / 2;
+
+        Console.WriteLine($"Result: {result}");
+    }
+}
